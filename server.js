2f634eceeca8ecf3e8d990f644bede327f942c77
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-10-12 14:25:37 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,46 @@
+/** 
+ * Arquivo: server.js
+ * Descrição: Arquivo responsável por levantar o serviço do Node.Js para poder
+ * executar a aplicação e a API através do Express.Js.
+ * Author: Glaucia Lemos
+ * Data de Criação: 12/09/2016
+ */
+
+//Configuração Base da Aplicação:
+//====================================================================================
+
+/* Chamada das Packages que iremos precisar para a nossa aplicação */
+var express     = require('express'); //chamando o pacote express
+var app         = express(); //definção da nossa aplicação através do express
+var bodyParser  = require('body-parser');  //chamando o pacote body-parser
+
+/** Configuração da variável 'app' para usar o 'bodyParser()'.
+ * Ao fazermos isso nos permitirá retornar os dados a partir de um POST
+ */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/** Definição da porta onde será executada a nossa aplicação */
+var port = process.env.PORT || 8000; 
+
+//Rotas da nossa API: 
+//==============================================================
+
+/* Aqui o 'router' irá pegar as instâncias das Rotas do Express */
+var router  = express.Router(); 
+
+/* Rota de Teste para sabermos se tudo está realmente funcionando (acessar através: GET: http://localhost:8000/api) */
+router.get('/', function(req, res) {
+    res.json({ message: 'YEAH! Seja Bem-Vindo a nossa API' });
+});
+
+/* TODO - Definir futuras rotas aqui!!! */
+
+
+/* Todas as nossas rotas serão prefixadas com '/api' */
+app.use('/api', router);
+
+//Iniciando o Servidor (Aplicação):
+//==============================================================
+app.listen(port);
+console.log('Iniciando a aplicação na porta ' + port);
